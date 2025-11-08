2203f0ee5db832a6a5530693d5294b2a06b756c4
yabu1121 <n.hayato0122@outlook.jp>
2025-11-09 00:51:13 +0900

    bubblesort

---
+++ argorithm/bubblesort.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// バブルソート
+// O(n^2)
+void bubble_sort(vector<int> &vec){
+  int temp;
+  int n = vec.size();
+  for(int i = 0; i < n - 1; i++){
+    for(int j = 0; j < n - 1 - i; j++){
+      if(vec[j] > vec[j + 1]){
+        temp = vec[j];
+        vec[j] = vec[j + 1];
+        vec[j + 1] = temp;
+      }
+    }
+  }
+}
+
+int main(){
+  vector<int> x = {5, 2, 4, 1, 3};
+  int n = x.size();
+  bubble_sort(x);
+  for(int i = 0; i < n; i++){
+    cout << x[i] << " ";
+  }
+}
