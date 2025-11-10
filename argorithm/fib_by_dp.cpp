0be391f22a38263fb6b1fb9ddb5b4130348c2bc4
yabu1121 <n.hayato0122@outlook.jp>
2025-11-10 16:38:15 +0900

    revise

--- argorithm/fib_by_dp.cpp
+++ argorithm/fib_by_dp.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 long long fib(long long n) {
-    long long a[100];
-    a[0] = 1;
-    a[1] = 1;
-    for(long long i = 2; i <= n; i += 1) {
-        a[i] = a[i-1] + a[i-2];
-    }
-    return a[n];
+  vector<long long> a(100);
+  a.at(0) = 1;
+  a.at(1) = 1;
+  for(long long i = 2; i <= n; i += 1) {
+    a.at(i) = a.at(i-1) + a.at(i-2);
+  }
+  return a[n];
 }
  
 int main() {
